a7d6e98ac3411b48792cc6f5a7174e66ec0c7a98
Christopher Wu <wu.christophert@gmail.com>
2020-04-19 16:50:57 -0700

    removed private docs, refactored split

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -9,25 +9,15 @@ defmodule WordCount do
     string_to_word_list(sentence) |> word_counter()
   end
 
-  @doc """
-  Transforms string into a list of downcase words
-
-  "Hi! how are you?" -> ["hi", "how", "are", "you"]
-  """
   @spec string_to_word_list(String.t()) :: list(String.t())
   defp string_to_word_list(string) do
     string
     |> String.downcase()
-    |> String.replace(~r/@|#|\$|%|&|\^|:|_|!|,/u, " ")
-    |> String.split(" ", trim: true)
+    |> String.split(~r/[^\p{L}|\p{Nd}|\p{Pd}]/u, trim: true)
   end
 
-  @doc """
-  Counts how many times each word appears in a list.
-  Returns a map where the key is the word and the value is the times it shows up.
-  """
   @spec word_counter(List.t()) :: map
-  def word_counter(word_list) do
+  defp word_counter(word_list) do
     Enum.reduce(word_list, Map.new(), fn word, map ->
       Map.update(map, word, 1, &(&1 + 1))
     end)
